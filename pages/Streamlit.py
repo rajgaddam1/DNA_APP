8887d27d3052de528d7ee5eca668383fcc12ff84
rajgaddam1 <119797412+rajgaddam1@users.noreply.github.com>
2022-12-20 17:41:54 +0530

    Update Streamlit.py

--- pages/Streamlit.py
+++ pages/Streamlit.py
@@ -667,8 +667,8 @@ if sql_window:
 #######HOME PAGE
 if sel_ware == '-------------------' and sel_data == '-------------------' and sel_role == '-------------------'  and sel_user == '-------------------' and sel_report == '-------------------' and not sql_window :
     st.title('SNOWFLAKE CLIENT')
-    sel_role1 = st.selectbox("Role", ['DCM_USER', 'ACCOUNTADMIN', 'ORGADMIN', 'PUBLIC', 'SYSADMIN'])
-    sel_ware1 = st.selectbox("User", ['DNAHCK_W', 'DNAHACK','SNOWFLAKE'])
+    sel_role1 = st.selectbox("Role", roles_df.name)
+    sel_ware1 = st.selectbox("Warehouse", wareshouse.name)
 
 
         
