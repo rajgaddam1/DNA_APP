7ead61c8a6804ec9d18fc0d30f17b4782e8e0f42
rajgaddam1 <119797412+rajgaddam1@users.noreply.github.com>
2022-12-20 16:09:44 +0530

    Update Streamlit.py

--- pages/Streamlit.py
+++ pages/Streamlit.py
@@ -629,9 +629,9 @@ if sql_window:
     st.title('SNOWFLAKE CLIENT ')
     sel_role2 = st.selectbox("Role ", roles_df.name)
     sel_ware2 = st.selectbox("Warehouse ", wareshouse.name)
-    buff, col, buff2 = st.columns([10,1,1])
-    sql_query1 = col.text_input('Enter SQL')
-    #sql_query1 = st.text_input('Enter SQL')
+    #buff, col, buff2 = st.columns([1,3,1])
+    #sql_query1 = col.text_input('Enter SQL')
+    sql_query1 = st.text_area('Enter SQL', height= 50)
     if st.button('Enter SQL'):
         pass
         
