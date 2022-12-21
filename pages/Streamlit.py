0b25920d646d0365a40a1357985b2c6c9dbb2ab8
rajgaddam1 <119797412+rajgaddam1@users.noreply.github.com>
2022-12-21 17:16:53 +0530

    Update Streamlit.py

--- pages/Streamlit.py
+++ pages/Streamlit.py
@@ -14,20 +14,6 @@ password = os.environ.get('password')
 account = os.environ.get('account')
 
 ############Check Log in
-
-
-
-try:
-    account_name_fin = st.session_state['account_name']
-    user_name1_fin = st.session_state['user_name1']
-    password1_fin = st.session_state['password1']
-    if user != user_name1_fin and password != password1_fin and account != account_name_fin:
-        st.warning("You must log-in to see the content of this sensitive page! Head over to the log-in page.")
-        st.stop()  # App won't run anything after this line
-except:
-     st.warning("Please Login")
-     st.stop()
-    
         
 ##To manage bug in sreamlit(Intialize button click)
 if 'key' not in st.session_state:
