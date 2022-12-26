9bce7543ab615866a6d9939a8e84bcef7d49b27a
rajgaddam1 <119797412+rajgaddam1@users.noreply.github.com>
2022-12-26 10:56:07 +0530

    Update Streamlit.py

--- pages/Streamlit.py
+++ pages/Streamlit.py
@@ -6,6 +6,22 @@ warnings.filterwarnings("ignore")
 import pandas as pd
 from snowflake.connector.connection import SnowflakeConnection
 from PIL import Image
+##########Header and Footer
+hide_footer_style='''
+<style>
+reportview-container .main footer (visibility: hidden;)
+'''
+st.markdown(hide_footer_style, unsafe_allow_html=True)
+
+hide_menu_style= '''
+<style>
+#MainMenu (visibility: hidden;}
+</style>
+'''
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+
+
+
 
 ##############Snowflake Credentials
 
