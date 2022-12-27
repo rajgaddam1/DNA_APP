6b2eeac350de01fcdfd90c700e25cf72da7e6662
rajgaddam1 <119797412+rajgaddam1@users.noreply.github.com>
2022-12-27 11:15:52 +0530

    Update Streamlit.py

--- pages/Streamlit.py
+++ pages/Streamlit.py
@@ -30,7 +30,7 @@ hide_menu_style= '''
 '''
 st.markdown(hide_menu_style, unsafe_allow_html=True)
 with st.sidebar:
-    st.markdown(f"'{<div class="markdown-text-container stText" style="width: 698px;"> <footer><p></p></footer><div style="font-size: 12px;">Snowflake Client v 0.1</div> <div style="font-size: 12px;">Infosys Technologies Limited</div></div>}'", 
+    st.markdown(f'"{<div class="markdown-text-container stText" style="width: 698px;"> <footer><p></p></footer><div style="font-size: 12px;">Snowflake Client v 0.1</div> <div style="font-size: 12px;">Infosys Technologies Limited</div></div>}"', 
                 unsafe_allow_html=True)
 
 ############Check Log in
