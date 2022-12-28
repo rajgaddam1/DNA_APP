7eb7d5e55608f9e27e24baf5af0f95bf74f9c242
rajgaddam1 <119797412+rajgaddam1@users.noreply.github.com>
2022-12-28 14:45:56 +0530

    Update Streamlit.py

--- pages/Streamlit.py
+++ pages/Streamlit.py
@@ -980,7 +980,7 @@ if sel_ware == '-------------------' and sel_data == '-------------------' and s
     chart_data = pd.DataFrame([a, b, c, d],columns=["Count", "Object"])
     bar_chart = alt.Chart(chart_data).mark_bar().encode(
         y = 'Count',
-        x = 'Object',
+        x = 'Object:N',
         color=alt.Color("Object:N", scale=scale),
         )
     st.altair_chart(bar_chart, theme=None, use_container_width=True)
